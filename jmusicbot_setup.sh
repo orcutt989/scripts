8c20d589bea8efea334020148fbb19eee799eff8
Jimmy <5608027+orcutt989@users.noreply.github.com>
2022-03-01 13:30:11 -0500

    Update jmusicbot_setup.sh

--- jmusicbot_setup.sh
+++ jmusicbot_setup.sh
@@ -3,19 +3,19 @@ yum install jq -y
 cd ~ || exit
 curl -s https://api.github.com/repos/jagrosh/musicbot/releases/latest | grep "browser_download_url.*jar" | cut -d : -f 2,3 | tr -d \" | wget -qi -
 
-filename=$(ls)
+FILENAME=$(ls)
 
-secret_names=(
+SECRET_NAMES=(
 "jmusicbot_discord_bot_token"
 "discord_id_owner"
 )
 
 EC2_REGION=`curl http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}'`
 
-for secret_name in "${secret_names[@]}"
+for SECRET_NAME in "${SECRET_NAME[@]}"
 do
-  secret=$(aws secretsmanager get-secret-value --region "${EC2_REGION}" --secret-id "${var}" --query SecretString --output text | jq -r ."${var}")
-  echo "${secret_name##*_*_} = ${secret}" >> config.txt
+  SECRET=$(aws secretsmanager get-secret-value --region "${EC2_REGION}" --secret-id "${SECRET_NAME}" --query SecretString --output text | jq -r ."${SECRET_NAME}")
+  echo "${SECRET_NAME##*_*_} = ${SECRET}" >> config.txt
 done
 
 echo "prefix = \"-\"" >> config.txt
